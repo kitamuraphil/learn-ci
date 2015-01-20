c7b41a6cd778b710ba56084755730d2efaaf2af9
@ProfNandaa <profnandaa@gmail.com>
2015-01-21 00:06:46 +0300

    Added jQuery

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -1,5 +1,6 @@
 </div>
 
+<script src="<?php echo base_url(); ?>assets/js/jquery-2.1.1.min.js"></script>
 <script src="<?php echo base_url();?>assets/bootstrap/js/bootstrap.min.js"></script>
 
 </body>
