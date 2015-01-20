6c449bed72e81c684959ddac3fff055a7fe097a0
@ProfNandaa <profnandaa@gmail.com>
2015-01-21 00:20:33 +0300

    Enhanced Bootstrap intergration

--- application/views/home/login.php
+++ application/views/home/login.php
@@ -10,7 +10,7 @@ if($this->session->flashdata("error") != ""){
 echo form_open("home/login/submit");
 echo form_input("email",set_value("email"),"placeholder='Email'");
 echo form_password("password","","placeholder='Password'");
-echo form_submit("login","Login");
+echo form_submit("login","Login","class='btn btn-success'");
 echo form_close();
 ?>
 
--- application/views/include/header.php
+++ application/views/include/header.php
@@ -8,18 +8,34 @@
   </head>
   <body>
   	<div class="main">
-  		<div class="menu">
-  			<ul>
-  				<li><?php echo anchor("home","Home"); ?></li>
-  			</li>
-  			<div class="user-caption">
-  				<?php
-  					if($this->session->userdata("username") != ""){
-  						echo $this->session->userdata("username");
-              echo " &bull; ".anchor("home/logout","Logout");
-  					}else{
-  						echo anchor("home/login","Login");
-  					}
-  				?>
-  			</div>
-  		</div>
+       <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Rental MS</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><?php echo anchor("home","Home"); ?></li>
+            <li><a href="#about">About</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li>
+              <?php
+                if($this->session->userdata("username") != ""){
+                  echo $this->session->userdata("username");
+                  echo " &bull; ".anchor("home/logout","Logout");
+                }else{
+                  echo anchor("home/login","Login");
+                }
+              ?>
+            </li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
